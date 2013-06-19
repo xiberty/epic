dd31862d1add55e039e28adc2db29a555c69c7dc
jvacx <jvacx.log@gmail.com>
2013-06-19 08:41:11 -0400

    cree la funcion getter

--- core/getter.php
+++ core/getter.php
@@ -1,3 +1,31 @@
 <?php 
+	include('config.php');
 
+	$table = $_POST['table'];
+	$cc = $_POST['cols'];
+	$temp = $_POST['temp'];
+
+	$cols = explode("|",$cc);
+
+	$sql = "SELECT * FROM ".$table; 
+	$result = mysql_query($sql,$con) or die('Error: Conexion  Fallida: '.mysql_error());
+
+	$fill = array( 'rows' => array(), 'temp' => $temp );
+
+	$item = array();
+
+	foreach ($cols as $col => $value) {
+		$item[ ''.$value ] = $value;
+	}
+
+	while ($res=mysql_fetch_array($result)){
+		
+		foreach ($cols as $col => $value) {
+			$item[ ''.$value ] = utf8_encode($res[ ''.$value ]);
+		}
+		
+		$fill['rows'][] = $item;
+	}	
+
+	echo json_encode($fill);	
  ?>
